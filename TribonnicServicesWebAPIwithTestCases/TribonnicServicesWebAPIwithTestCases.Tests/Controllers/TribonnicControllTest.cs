4628fc8ebda22549ce4162f9c5e4d0f48e0b052e
ramandeep <ramandeep738@gmail.com>
2016-09-01 16:38:31 +0530

    TestCodeWritten

---
+++ TribonnicServicesWebAPIwithTestCases/TribonnicServicesWebAPIwithTestCases.Tests/Controllers/TribonnicControllTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TribonnicServicesWebAPIwithTestCases.Tests.Controllers
+{
+    [TestClass]
+    public class TribonnicControllTest
+    {
+        [TestMethod]
+        public void TestTribonnicSeries()
+        {
+            // Arrange
+            TribonnicController controller = new TribonnicController();
+
+            // Act
+            IEnumerable<int> result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(20, result.Count());
+            Assert.AreEqual(3, result.ElementAt(3));
+            Assert.AreEqual(9, result.ElementAt(5));
+
+        }
+
+        [TestMethod]
+        public void TestTribonnicSeriesDivisiblePositionN(int division, int position)
+        {
+            // Arrange
+            TribonnicController controller = new TribonnicController();
+
+            // Act
+            int result = controller.GetbyDivisionPosition(division,position);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(355, result);
+            
+        }
+    }
+}
